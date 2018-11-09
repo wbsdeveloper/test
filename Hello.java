854e2bc7a57b92e594c8d38c56e11b5106369f3d
wbs <wbssupervisor@163.com>
2018-11-09 10:52:51 +0800

    add Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+﻿public class Hello{
+    public static void main(String[] args) {
+		 System.out.println("Hello Java");
+
+		 System.out.println("中华人民共和国");
+
+		 System.out.println("中华人民共和国");
+
+		
+
+		
+			
+	}
+
+
+}
