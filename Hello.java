c282f6597d395f6debbc63629d8c2bcdbed0a462
wbs <wbssupervisor@163.com>
2018-11-09 11:32:12 +0800

    Add new file Admin.java And edit Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,12 @@
-﻿public class Hello{
+public class Hello{
     public static void main(String[] args) {
 		 System.out.println("Hello Java");
 
 		 System.out.println("中华人民共和国");
 
 		 System.out.println("中华人民共和国");
+
+		 System.out.println("在dev分支上修改了此文件内容。。。");
 
  		
 		
