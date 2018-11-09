76eb640c901370ddc54dd3fdcf813e0bea1f124c
wbs <wbssupervisor@163.com>
2018-11-09 11:06:04 +0800

    Add new file Admin.java And edit Hello.java

---
+++ Admin.java
@@ -0,0 +1,7 @@
+public class Admin{
+  public static void main(String[] args) {
+		 System.out.println("这是在dev分支上增加的Admin.java);		
+	}
+
+ 
+}
--- Hello.java
+++ Hello.java
@@ -7,7 +7,7 @@
 		 System.out.println("中华人民共和国");
 
 		
-
+		 System.out.println("在dev分支上修改了此文件内容。。。");
 		
 			
 	}
